880d0aef7df95e8e28fc3e13920caa727f984597
ricardocarreras <92594218+ricardocarreras@users.noreply.github.com>
2024-02-01 00:05:39 +0100

    ten commit

--- app2.py
+++ app2.py
@@ -14,11 +14,11 @@ load_dotenv()
 # Asignar la API KEY de OpenAI
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def generar_publicacion_con_gpt(tema, palabras_clave, matiz):
+def generar_publicacion_con_gpt(tema, palabras_clave, matiz, fechahoy, urlnoticia):
     try:
         client = OpenAI(api_key=openai.api_key)
 
-        prompt = f"Escribe una noticia periodística de 1000 palabras sobre {tema}. Considera que {matiz}. Para crear el comienzo de la noticia busca en internet sobre ese mismo tema tres(3) noticias cuya fecha de publicación (escrita en la noticia) sea de las últimas 48 horas y que sean relevantes. Comienza la creación de tu nueva noticia por un resumen de 400 palabras de esas 3 noticias que has encontrado en internet. Escribes el resto de la noticia con texto relevante, hasta llegar a las 1000 palabras. La noticia que creas debe estar optimizada para las siguientes palabras clave: {palabras_clave}. La densidad de cada una de esas palabras clave debe ser del 2%, es decir que por cada 100 palabras del texto, la palabra clave o un sinónimo aparece 2 veces. Asegúrate de que los datos que das en la noticia son reales y verídicos, por favor no sufras alucinaciones ni inventes datos que no son reales"
+        prompt = f"Escribe una noticia periodística de 1000 palabras sobre {tema}. Considera que hoy es {fechahoy}. Considera que {matiz}. Para crear la noticia comienza por realizar un resumen de esta noticia que puedes encontrar en esta url {urlnoticia}. Después agrega más texto sintetizando otras noticias que puedas encontrar en internet sobre ese mismo tema, con fecha de publicación (escrita en la noticia) sea de las últimas 48 horas y que sean relevantes. Escribe el resto de la noticia con texto relevante, hasta llegar a las 1000 palabras. La noticia que creas debe estar optimizada para las siguientes palabras clave: {palabras_clave}. La densidad de cada una de esas palabras clave debe ser del 2%, es decir que por cada 100 palabras del texto, la palabra clave o un sinónimo aparece 2 veces. Asegúrate de que los datos que das en la noticia son reales y verídicos, por favor no sufras alucinaciones ni inventes datos que no son reales"
 
         response = client.completions.create(
             model="gpt-3.5-turbo-instruct",
@@ -35,9 +35,12 @@ st.title("Generador de Noticias Optimizadas")
 tema = st.text_input("Ingresa el tema de la noticia:")
 palabras_clave = st.text_input("Ingresa las palabras clave (separadas por comas):")
 matiz = st.text_input("Ingresa el matiz que consideres oportuno añadir:")
+fechahoy = st.text_input("Ingresa la fecha de hoy:")
+urlnoticia = st.text_input("Ingresa la url de la noticia inicial:")
+
 
 if st.button("Generar Publicación"):
-    publicacion = generar_publicacion_con_gpt(tema, palabras_clave, matiz)
+    publicacion = generar_publicacion_con_gpt(tema, palabras_clave, matiz, fechahoy, urlnoticia)
     st.text_area("Publicación Generada:", publicacion, height=300)
 
 
