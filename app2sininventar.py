aaa47b3cf335b2ec41f144115686446b1f716d53
ricardocarreras <92594218+ricardocarreras@users.noreply.github.com>
2024-09-01 20:40:52 +0200

    Fist commit

---
+++ app2sininventar.py
@@ -0,0 +1,49 @@
+import streamlit as st
+
+import openai
+from openai import OpenAI
+
+# Configura tu clave API de OpenAI
+import os
+
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Asignar la API KEY de OpenAI
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+def generar_publicacion_con_gpt(tema, palabras_clave, matiz, fechahoy, urlnoticia1, urlnoticia2):
+    try:
+        client = OpenAI(api_key=openai.api_key)
+
+        prompt = f"Escribe una noticia periodística de 1000 palabras sobre {tema}. Considera que hoy es {fechahoy}. Considera que {matiz}. Para crear la noticia comienza por fusionar dos noticias, la primera la puedes encontrar en esta url {urlnoticia1}. La segunda en esta otra url {urlnoticia2}. Fusiona esas dos noticias en un texto de 500 palabras. Después agrega más texto relevante sobre el tema, hasta llegar a las 1000 palabras. La noticia que escribes debe estar optimizada para las siguientes palabras clave: {palabras_clave}. La densidad de cada una de esas palabras clave debe ser del 2%, es decir que por cada 100 palabras del texto, la palabra clave o un sinónimo aparece 2 veces. Asegúrate de que los datos que das en la noticia son reales y verídicos, por favor no sufras alucinaciones ni inventes datos que no son reales. Sobre todo, no incluyas citas inventadas de personas que no existen o citas inventadas que no están publicadas"
+
+        response = client.completions.create(
+            model="gpt-3.5-turbo-instruct",
+            prompt=prompt,
+            temperature=0.1,
+            top_p=0.5,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+            max_tokens=3300  # Ajusta según la longitud deseada
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        return f"Error al generar la publicación: {e}"
+
+# Interfaz de Streamlit
+st.title("Generador de Noticias Optimizadas")
+
+tema = st.text_input("Ingresa el tema de la noticia:")
+palabras_clave = st.text_input("Ingresa las palabras clave (separadas por comas):")
+matiz = st.text_input("Ingresa el matiz que consideres oportuno añadir:")
+fechahoy = st.text_input("Ingresa la fecha de hoy:")
+urlnoticia1 = st.text_input("Ingresa la url de la noticia inicial:")
+urlnoticia2 = st.text_input("Ingresa la url de la segunda noticia:")
+
+if st.button("Generar Publicación"):
+    publicacion = generar_publicacion_con_gpt(tema, palabras_clave, matiz, fechahoy, urlnoticia1, urlnoticia2)
+    st.text_area("Publicación Generada:", publicacion, height=300)
+
